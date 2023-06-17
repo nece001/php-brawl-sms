d9a4f2228d79cd7f9f26c978c55811573bfc0c0d
nece001 <nece001@163.com>
2023-06-17 10:35:45 +0800

    '调整命名空间计算'

--- src/Factory.php
+++ src/Factory.php
@@ -2,18 +2,57 @@
 
 namespace Nece\Brawl\Sms;
 
+use Error;
 use Nece\Brawl\ConfigAbstract;
 use ReflectionClass;
 
+/**
+ * 实例工厂
+ *
+ * @Author nece001@163.com
+ * @DateTime 2023-06-17
+ */
 class Factory
 {
-    public static function create(ConfigAbstract $config)
+    /**
+     * 创建配置对象
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-17
+     *
+     * @param string $provider 服务商名
+     *
+     * @return \Nece\Brawl\ConfigAbstract
+     */
+    public static function createConfig($provider)
+    {
+        $class = __NAMESPACE__ . '\\' . ucfirst($provider) . '\\Config';
+        return new $class();
+    }
+
+    /**
+     * 创建客户端
+     *
+     * @Author nece001@163.com
+     * @DateTime 2023-06-17
+     *
+     * @param ConfigAbstract $config
+     *
+     * @return \Nece\Brawl\Sms\SenderAbstract
+     */
+    public static function createClient(ConfigAbstract $config)
     {
         $class = new ReflectionClass($config);
         $namespace = $class->getNamespaceName();
         $parts = explode('\\', $namespace);
-        $parts[count($parts) - 1] = 'Sender';
-
-        
+        $parts[] = 'Sender';
+        try {
+            $class = implode('\\', $parts);
+            $instance = new $class();
+            $instance->setConfig($config);
+            return $instance;
+        } catch (Error $e) {
+            throw new SmsException('不支持的短信发送器');
+        }
     }
 }
